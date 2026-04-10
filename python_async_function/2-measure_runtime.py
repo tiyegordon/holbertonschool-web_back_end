115ab1eb5a203058c88be0a24f99c1c29850f02e
tiyegordon <tiye.gordon@gmail.com>
2026-04-10 17:54:17 -0400

    updated

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-""" Takes 2 int args, waits for random delay """
+""" Measures total execution time """
 
 import asyncio
 import random
-from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """ Waits for ran delay until max_delay, returns list of actual delays """
-    spawn_list = []
-    delay_list = []
-    for i in range(n):
-        delayed_task = asyncio.create_task(wait_random(max_delay))
-        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(delayed_task)
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """ Returns total_time / n for wait_n() execution """
 
-    for spawn in spawn_list:
-        await spawn
+    elapsed_time: float
 
-    return delay_list
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    elapsed_time = time.perf_counter() - start_time
+    return elapsed_time / n
