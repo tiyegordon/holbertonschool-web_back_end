f9ee8c15cb0e120469469e1397601b0886565946
tiyegordon <tiye.gordon@gmail.com>
2026-04-10 17:45:14 -0400

    main.py files

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-"""Collect values from an asynchronous generator with a comprehension."""
+""" Takes 2 int args, waits for random delay """
 
+import asyncio
+import random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-async_generator = __import__("0-async_generator").async_generator
 
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_list = []
+    delay_list = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
 
-async def async_comprehension() -> List[float]:
-    """Return ten random floats gathered from async_generator."""
-    return [value async for value in async_generator()]
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
