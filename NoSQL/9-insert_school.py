adae5055e2e50b4b5640a214329e6d73d5f3df56
tiyegordon <tiye.gordon@gmail.com>
2026-04-24 23:22:47 -0400

    task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that inserts a new document in a collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection: pymongo collection object
+        **kwargs: key/value pairs to insert as document attributes
+
+    Returns:
+        The new _id of the inserted document
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
