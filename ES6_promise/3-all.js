ee1cf1ef1cc1a406f1d333629843e1ac4c1af06d
tiyegordon <tiye.gordon@gmail.com>
2026-05-08 21:29:59 -0400

    task 3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (e) {
+    console.log('Signup system offline');
+  }
+}
