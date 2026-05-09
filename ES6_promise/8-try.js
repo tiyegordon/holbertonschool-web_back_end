9ef6aa5ee1e69f06c6550ffdfea8e3538531ea38
tiyegordon <tiye.gordon@gmail.com>
2026-05-08 21:34:34 -0400

    task 8

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
+}
