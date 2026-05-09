db7341e94bb19a62babe3a63894290580a65c980
tiyegordon <tiye.gordon@gmail.com>
2026-05-08 21:28:55 -0400

    task 2

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
