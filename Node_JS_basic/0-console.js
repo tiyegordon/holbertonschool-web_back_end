be62a17f822011e6858f85d15aa4559537f08af1
tiyegordon <tiye.gordon@gmail.com>
2026-05-14 21:08:07 -0400

    task 0

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// Create a function named displayMessage that prints in STDOUT the string argument.
+
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
